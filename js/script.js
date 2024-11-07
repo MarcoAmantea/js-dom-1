634c2c009f67c1b3ad7a158861c5ce78e23d9328
MarcoAmantea <marcoamantea18@gmail.com>
2024-11-07 16:21:48 +0100

    feat: bonus

--- js/script.js
+++ js/script.js
@@ -5,8 +5,15 @@ const btnOff = document.getElementById("btn-off");
 console.log(btnOff);
 
 btnOff.addEventListener ("click", function(){
-    imgOff.src = "../img/yellow_lamp.png"
-    btnOff.innerHTML = "SPEGNI LA LUCE!"
+    if(btnOff.classList.contains("on")){
+        btnOff.classList.remove("on");
+        imgOff.src = "../img/yellow_lamp.png";
+        btnOff.innerHTML = "SPEGNIMI TI PREGO! MI STO SURRISCALDANDO!!";
+    }else{
+      btnOff.classList.add("on");
+      imgOff.src = "../img/white_lamp.png";
+      btnOff.innerHTML = "ACCENDIMI DAI!";
+    }
 })
 
 
